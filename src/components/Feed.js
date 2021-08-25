df2cbd798fdb28c518b75de25a54db9d820d8f57
sasank <gsbnsrsasank@gmail.com>
2021-08-25 20:53:19 +0530

    message sender structure

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import StoryReel from './StoryReel'
 import './Feed.css';
+import MessageSender from './MessageSender';
 
 const Feed = () => {
   return (
     <div className="feed"> 
       <StoryReel/>
+      <MessageSender/>
     </div>
   )
 }
